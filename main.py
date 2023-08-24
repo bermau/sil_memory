33ee7993fb9a44fd653bf869d7565b99191d4911
bertrand <bertrand.maubert@ch-lepuy.fr>
2023-08-24 16:32:52 +0200

    Le df peut être ordonné puis exporté en csv.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Etude des messages de Glims
 import re
+import pandas as pd
 
 data = {
     "08/05/2022": """'Schema Area' (db glims) est presque plein (284588 blocks libres,  97.4 % utilisé)
@@ -23,8 +24,10 @@ Il n'y a que 3281 MB libre dans cet environnement
 Veuillez ajouter un ou plusieurs DB extents à l'environnement 'Schema Area'"""
 }
 
+order = ['vol_disque', 'vol_disp_MB', 'vol_occup%', "area", 'blk_libres', 'area_occup%', 'area_MB_libres']
 
 class Extracteur:
+
 
     def __init__(self, date_str, txt):
         self.date = date_str
@@ -58,9 +61,9 @@ class Extracteur:
     def extraire_espace_disque(a_line):
         """Phrase est de type :
         Il n'y a pas assez d'espace disque sur /glimsdbf/.Il ne reste que 10413 MB, 91.85% complet"""
-        print("Ligne est : ", a_line)
         corresp = re.search(r"(/.*?/)\.Il ne reste que (\d+).*(\d+\.\d+)%.*", a_line)
-        return {"volume_disque": corresp.group(1), "Dispo_MB": corresp.group(2), 'occup%': corresp.group(3)}
+        return {'vol_disque': corresp.group(1), 'vol_disp_MB': corresp.group(2), 'vol_occup%': corresp.group(3)}
+
 
     @staticmethod
     def extraire_schema_area(lines):
@@ -69,12 +72,28 @@ class Extracteur:
 Il n'y a que 3281 MB libre dans cet environnement
 Veuillez ajouter un ou plusieurs DB extents à l'environnement 'Schema Area'
 """
-        print(lines)
         cor_line1 = re.search(r".*\((.*?)\).*(\d+).*(\d+)", lines[0])
         cor_line2 = re.search(r"(\d+) MB libre", lines[1])
         return {"area": cor_line1.group(1), 'blk_libres': cor_line1.group(2), 'area_occup%': cor_line1.group(3),
-                'MB_libres': cor_line2.group(1)
+                'area_MB_libres': cor_line2.group(1)
                 }
+
+class DataIntegrator:
+    """A class to create structure like pandas Dataframe from dictionaries"""
+    def __init__(self):
+        self.df = pd.DataFrame()
+    def add_line(self, index, dico):
+        """Ajoute une ligne au tableau à partir d'un dictionnaire décrivant la ligne"""
+        line_df = pd.DataFrame.from_records(dico, index = [index]         )
+        self.df = pd.concat([self.df, line_df], axis=0)
+        # self.df.concat(line_df, axis = 0)
+        print(self.df)
+
+    def order_columns(self):
+        self.df = self.df.reindex(order, axis=1)
+
+    def to_csv(self):
+        self.df.to_csv("out.csv")
 
 
 # Press the green button in the gutter to run the script.
@@ -82,7 +101,19 @@ if __name__ == '__main__':
     record = "08/12/2022"
     E = Extracteur(record, data[record])
     print(E.analyse())
+    #
+    # print("\nEtude pour l'extraction")
+    # for record in list(data)[:2]:
+    #     E = Extracteur(record, data[record])
+    #     print(E.analyse())
 
-    for record in data:
-        E = Extracteur(record, data[record])
-        print(E.analyse())
+    print("\nEtude pour l'intégration")
+    I = DataIntegrator()
+    for date in ["08/05/2022",  "16/10/2022", "08/12/2022"]:
+        E = Extracteur(date, data[date])
+        dico = E.analyse()
+        I.add_line(date, dico)
+    I.order_columns()
+    I.to_csv()
+    print(I.df)
+
