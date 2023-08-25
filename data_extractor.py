0771f6e7c454ad6195a2586e1180ca9e3a0b5b77
bertrand <bertrand.maubert@ch-lepuy.fr>
2023-08-25 09:45:58 +0200

    Correction d'une regex pour extraire une valeur numérique.

--- data_extractor.py
+++ data_extractor.py
@@ -61,7 +61,7 @@ class Extracteur:
     def extraire_espace_disque(a_line):
         """Phrase est de type :
         Il n'y a pas assez d'espace disque sur /glimsdbf/.Il ne reste que 10413 MB, 91.85% complet"""
-        corresp = re.search(r"(/.*?/)\.Il ne reste que (\d+).*(\d+\.\d+)%.*", a_line)
+        corresp = re.search(r"(/.*?/)\.Il ne reste que (\d+) MB, (.*)%.*", a_line)
         return {'vol_disque': corresp.group(1), 'vol_disp_MB': corresp.group(2), 'vol_occup%': corresp.group(3)}
 
 
